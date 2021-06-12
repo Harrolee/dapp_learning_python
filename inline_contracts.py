17310aee11ca991fa8372d802a2208753f8fe9ae
= <lharrold@scu.edu>
2021-06-11 22:17:13 -0700

    first commit

--- inline_contracts.py
+++ inline_contracts.py
@@ -40,3 +40,33 @@ compiled_sol = compile_standard({
             }
         }
     })
+
+# fiddle with these arguments: https://docs.soliditylang.org/en/develop/using-the-compiler.html#compiler-input-and-output-json-description
+compiled_sol_nft = compile_standard({
+    "language": "Solidity",
+    "sources": {
+        "MyNFT.sol": { 
+            "urls":
+                [
+                    # how will the ipfs url be used??
+                    
+                    #"bzzr://56ab...",
+                    #"ipfs://Qma...",
+                    "contracts/MyNFT.sol"
+                    #// If files are used, their directories should be added to the command line via
+                    #// `--allow-paths <path>`.
+                ]
+        }
+    },
+    "settings":
+        {
+            "outputSelection": {
+                "*": {
+                    "*": [
+                        "metadata", "evm.bytecode"
+                        , "evm.bytecode.sourceMap"
+                    ]
+                }
+            }
+        }
+    })
